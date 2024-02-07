7df416626ec730339506a79a24bb8df60a60cd79
Meenusri23 <158264977+Meenusri23@users.noreply.github.com>
2024-02-07 21:42:18 +0530

    First Commit

---
+++ JenkinsGIT_Example.java
@@ -0,0 +1,11 @@
+public class JenkinsGIT_Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Integrating Jenkins with GIT");
+		
+
+	}
+
+}
