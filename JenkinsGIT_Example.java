da69a5fefb997554fa8653bbb67c4cdc8727f8cd
Meenusri23 <158264977+Meenusri23@users.noreply.github.com>
2024-02-07 23:13:11 +0530

    Updated Git print message

--- JenkinsGIT_Example.java
+++ JenkinsGIT_Example.java
@@ -4,6 +4,7 @@ public class JenkinsGIT_Example {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Integrating Jenkins with GIT");
+		System.out.println("Checking Integration of jenkins with GIT");//newly added
 		
 
 	}
